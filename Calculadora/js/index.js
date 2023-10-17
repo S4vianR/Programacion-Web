a5a2b9c41bd40469bd40c8f9a9e8c50eb223ebb5
S4vianR <reuben4.com@gmail.com>
2023-10-16 18:30:34 -0600

    Commit

--- Calculadora/js/index.js
+++ Calculadora/js/index.js
@@ -47,3 +47,36 @@ const evaluate = (op) => {
         }
     }
 }
+
+
+/* 
+//Para hacerlo una funcion normal 
+
+function evaluate(op) {
+    let v1, v2, res;
+    v1 = parseFloat(document.getElementById("number1").value);
+    v2 = parseFloat(document.getElementById("number2").value);
+    res = document.getElementById("result");
+    if (isNaN(v1) || isNaN(v2)) {
+        res.style.color = "red";
+        res.value = "Error";
+        return;
+    } else {
+        res.style.color = "black";
+        switch (op) {
+            case 1:
+                res.value = v1 + v2;
+                break;
+            case 2:
+                res.value = v1 - v2;
+                break;
+            case 3:
+                res.value = v1 * v2;
+                break;
+            case 4:
+                res.value = v1 / v2;
+                break;
+        }
+    }
+}
+*/
